f6422015e73441b9608d6c96afbb1789cb322d25
GideobOgbuagu <reachgiddy@gmail.com>
2021-06-20 21:47:19 -0400

    created jsx & css for modal overlay

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Dropdown from "../src/components/Dropdown"
+import Dropdown from "../src/components/Dropdown";
+import Modal from "./components/Modal"
 import "./App.css";
 import topImage1 from "./assets/top-section-images/top-image-1.jpg";
 // import topImage2 from "./assets/top-section-images/top-image-2.jpg";
@@ -14,133 +15,139 @@ import sectnTwoImage from "./assets/bottom-section-images/sectiontwo-img.jpg";
 import sectnThreeImage1 from "./assets/bottom-section-images/sectionthree-img-1.jpg";
 import sectnThreeImage2 from "./assets/bottom-section-images/sectionthree-img-2.jpg";
 
-
-
-
 function App() {
-  const [selected, setSelected] = useState("")
-  const [black, setBlack] = useState(true)
-  const [beige, setBeige] = useState(false)
-  const [color, setColor] 
-        = useState({
-          black: true,
-          beige: false
-  })
-  const [quantity, setQuantity] = useState(1)
-  
+  const [selected, setSelected] = useState("");
+  const [black, setBlack] = useState(true);
+  const [beige, setBeige] = useState(false);
+  const [color, setColor] = useState({
+    black: true,
+    beige: false,
+  });
+  const [quantity, setQuantity] = useState(1);
+
   const increment = () => {
-    setQuantity(quantity + 1)
-  }
+    setQuantity(quantity + 1);
+  };
 
   const decrement = () => {
-    quantity > 1 && setQuantity(quantity - 1)
-  }
-  
+    quantity > 1 && setQuantity(quantity - 1);
+  };
+
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-          {" "}
-          {/* header starts here */}
-          <div className="header-wrapper">
-            <div className="top-description">
-              <p>HI - WAIST</p>
-              <p>Heavy days. Holds up to 2 tampon's worth</p>
-              <p>The phrase "treat yo'self" reaches new belly-button brushing heights with the Hi-Waist, and it's here just in time for cutting season.</p>
-            </div>
-            <div className="top-images">
-              <img className="top-image-1" src={topImage1} alt="HI WAIST (ONE)" />
-              {/* <img className="top-image-2" src={topImage2} alt="HI WAIST (TWO)"/>
+    <div>
+      <Modal />
+      <div className="App">
+          <header className="App-header">
+            {" "}
+            {/* header starts here */}
+            <div className="header-wrapper">
+              <div className="top-description">
+                <p>HI - WAIST</p>
+                <p>Heavy days. Holds up to 2 tampon's worth</p>
+                <p>The phrase "treat yo'self" reaches new belly-button brushing heights with the Hi-Waist, and it's here just in time for cutting season.</p>
+              </div>
+              <div className="top-images">
+                <img className="top-image-1" src={topImage1} alt="HI WAIST (ONE)" />
+                {/* <img className="top-image-2" src={topImage2} alt="HI WAIST (TWO)"/>
             <img className="top-image-3" src={topImage3} alt="HI WAIST (THREE)"/>
             <img className="top-image-4" src={topImage4} alt="HI WAIST (FOUR)"/>
             <img className="top-image-5" src={topImage5} alt="HI WAIST (FIVE)"/>
             <img className="top-image-6" src={topImage6} alt="HI WAIST (SIX)"/> */}
-            </div>
-            <div className="add-to-cart-wrapper">
-              <p>$35.00</p>
-              <div className="features-wrapper">
-                <div className="color-wrapper">
-                  {/* Color toggle functionality */}
-                  <span>color</span>
-                  <span onClick={(e) => {
-                    setBlack(true)
-                    setBeige(false)
-                    }} className={black ? "check": null}>black 
-                  </span>
-                  <span onClick={() => {
-                    setBeige(true)
-                    setBlack(false)
-                  }} className={beige ? "check": null}>beige</span>
-                </div>
-                {/* quantity increment and decrement functionality */}
-                <div className="quantity-wrapper">
-                  <span onClick={decrement}>-</span>
-                  <span> quantity ({quantity}) </span>
-                  <span onClick={increment}>+</span>
-                </div>
+              </div>
+              <div className="add-to-cart-wrapper">
+                <p>$35.00</p>
+                <div className="features-wrapper">
+                  <div className="color-wrapper">
+                    {/* Color toggle functionality */}
+                    <span>color</span>
+                    <span
+                      onClick={(e) => {
+                        setBlack(true);
+                        setBeige(false);
+                      }}
+                      className={black ? "check" : null}
+                    >
+                      black
+                    </span>
+                    <span
+                      onClick={() => {
+                        setBeige(true);
+                        setBlack(false);
+                      }}
+                      className={beige ? "check" : null}
+                    >
+                      beige
+                    </span>
+                  </div>
+                  {/* quantity increment and decrement functionality */}
+                  <div className="quantity-wrapper">
+                    <span onClick={decrement}>-</span>
+                    <span> quantity ({quantity}) </span>
+                    <span onClick={increment}>+</span>
+                  </div>
 
-                <Dropdown selected={selected} setSelected={setSelected}/>
-                <p className="size-question">what's my size?</p>
+                  <Dropdown selected={selected} setSelected={setSelected} />
+                  <p className="size-question">what's my size?</p>
+                </div>
+                <div>add to cart</div>
+                <p>build a cycle set and save up to 20%</p>
+                {/* <i class="fas fa-user"></i>  */}
               </div>
-              <div>add to cart</div>
-              <p>build a cycle set and save up to 20%</p>
-              {/* <i class="fas fa-user"></i>  */}
             </div>
+          </header>{" "}
+          {/* header ends here */}
+          <div className="section-container">
+            <section className="first-section">
+              {" "}
+              {/* first section starts here */}
+              <div className="section1-card1">
+                <img className="sectionone-img-1" src={sectnOneImage1} alt="Hiphugger" />
+                <div className="card-img-descrip">
+                  <p>Shop Hiphugger</p>
+                  <p>heavy days</p>
+                </div>
+              </div>
+              <div className="section1-card2">
+                <img className="sectionone-img-2" src={sectnOneImage2} alt="Cheeky" />
+                <div className="card-img-descrip">
+                  <p>Shop Cheeky</p>
+                  <p>light days</p>
+                </div>
+              </div>
+            </section>{" "}
+            {/* first section ends here */}
+            <section className="second-section">
+              {" "}
+              {/* second section starts here */}
+              <div className="section2-card">
+                <img className="sectiontwo-img-2" src={sectnTwoImage} alt="Hiphugger" />
+                <div className="card-img-descrip">
+                  <p>Shop Thong</p>
+                  <p>lightest days</p>
+                </div>
+              </div>
+            </section>{" "}
+            {/* second section ends here */}
+            <section className="first-section">
+              {" "}
+              {/* third section starts here */}
+              <div className="section1-card1">
+                <img className="sectionone-img-1" src={sectnThreeImage1} alt="Hiphugger" />
+                <div className="card-img-descrip">
+                  <p>Shop Sport</p>
+                  <p>medium days</p>
+                </div>
+              </div>
+              <div className="section1-card2">
+                <img className="sectionone-img-2" src={sectnThreeImage2} alt="Cheeky" />
+                <div className="card-img-descrip">
+                  <p>Shop Boyshort</p>
+                  <p>light days</p>
+                </div>
+              </div>
+            </section>{" "}
+            {/* third section ends here */}
           </div>
-        </header>{" "}
-        {/* header ends here */}
-        <div className="section-container">
-          <section className="first-section">
-            {" "}
-            {/* first section starts here */}
-            <div className="section1-card1">
-              <img className="sectionone-img-1" src={sectnOneImage1} alt="Hiphugger" />
-              <div className="card-img-descrip">
-                <p>Shop Hiphugger</p>
-                <p>heavy days</p>
-              </div>
-            </div>
-            <div className="section1-card2">
-              <img className="sectionone-img-2" src={sectnOneImage2} alt="Cheeky" />
-              <div className="card-img-descrip">
-                <p>Shop Cheeky</p>
-                <p>light days</p>
-              </div>
-            </div>
-          </section>{" "}
-          {/* first section ends here */}
-          <section className="second-section">
-            {" "}
-            {/* second section starts here */}
-            <div className="section2-card">
-              <img className="sectiontwo-img-2" src={sectnTwoImage} alt="Hiphugger" />
-              <div className="card-img-descrip">
-                <p>Shop Thong</p>
-                <p>lightest days</p>
-              </div>
-            </div>
-          </section>{" "}
-          {/* second section ends here */}
-          <section className="first-section">
-            {" "}
-            {/* third section starts here */}
-            <div className="section1-card1">
-              <img className="sectionone-img-1" src={sectnThreeImage1} alt="Hiphugger" />
-              <div className="card-img-descrip">
-                <p>Shop Sport</p>
-                <p>medium days</p>
-              </div>
-            </div>
-            <div className="section1-card2">
-              <img className="sectionone-img-2" src={sectnThreeImage2} alt="Cheeky" />
-              <div className="card-img-descrip">
-                <p>Shop Boyshort</p>
-                <p>light days</p>
-              </div>
-            </div>
-          </section>{" "}
-          {/* third section ends here */}
-        </div>
       </div>
     </div>
   );
---
+++ src/components/Modal.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Modal.css';
+
+import topImage1 from "../assets/top-section-images/top-image-1.jpg";
+import topImage2 from "../assets/top-section-images/top-image-2.jpg";
+import topImage3 from "../assets/top-section-images/top-image-3.jpg";
+import topImage4 from "../assets/top-section-images/top-image-4.jpg";
+import topImage5 from "../assets/top-section-images/top-image-5.jpg";
+import topImage6 from "../assets/top-section-images/top-image-6.jpg";
+
+function Modal(props) {
+  
+
+  return (
+    <div className="modal-container">
+      <span className="close-btn">X</span>
+      <div className="image-wrapper">
+            <img src={topImage1} className="image" alt="FIRST"/>
+            <img src={topImage2} className="image" alt="FIRST"/>
+            <img src={topImage3} className="image" alt="FIRST"/>
+            <img src={topImage4} className="image" alt="FIRST"/>
+            <img src={topImage5} className="image" alt="FIRST"/>
+            <img src={topImage6} className="image" alt="FIRST"/>
+      </div>
+     
+    </div>
+  )
+}
+
+export default Modal;
