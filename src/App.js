3c40121ec6809c9559be181cdd87e8a88f1f14c6
GideobOgbuagu <reachgiddy@gmail.com>
2021-09-27 09:55:28 -0400

    reduced header font size

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Colors from "./components/Colors";
 import Quantity from "./components/Quantity";
 import Dropdown from "../src/components/Dropdown";
